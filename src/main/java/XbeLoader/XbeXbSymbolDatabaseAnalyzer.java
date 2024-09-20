509c3696ea593f58c96cb1b9406a82adc94670a0
RadWolfie <RadWolfie@users.noreply.github.com>
2024-09-20 15:42:44 +0000

    add extend information support from xbsdb tool

--- src/main/java/XbeLoader/XbeXbSymbolDatabaseAnalyzer.java
+++ src/main/java/XbeLoader/XbeXbSymbolDatabaseAnalyzer.java
@@ -26,6 +26,7 @@ import java.io.FileNotFoundException;
 import generic.jar.ResourceFile;
 import ghidra.GhidraApplicationLayout;
 import ghidra.app.services.AbstractAnalyzer;
+import ghidra.app.services.AnalysisPriority;
 import ghidra.app.services.AnalyzerType;
 import ghidra.app.util.importer.MessageLog;
 import ghidra.framework.Application;
@@ -34,7 +35,20 @@ import ghidra.framework.OperatingSystem;
 import ghidra.framework.options.Options;
 import ghidra.program.flatapi.FlatProgramAPI;
 import ghidra.program.model.address.AddressSetView;
+import ghidra.program.model.data.DataType;
+import ghidra.program.model.data.Enum;
+import ghidra.program.model.data.GenericCallingConvention;
+import ghidra.program.model.data.Undefined;
+import ghidra.program.model.lang.Register;
+import ghidra.program.model.lang.CompilerSpec;
+import ghidra.program.model.listing.Function;
+import ghidra.program.model.listing.Function.FunctionUpdateType;
+import ghidra.program.model.listing.Parameter;
+import ghidra.program.model.listing.ParameterImpl;
 import ghidra.program.model.listing.Program;
+import ghidra.program.model.listing.Variable;
+import ghidra.program.model.listing.VariableFilter;
+import ghidra.program.model.listing.VariableStorage;
 import ghidra.program.model.symbol.SourceType;
 import ghidra.program.model.symbol.Namespace;
 import ghidra.util.exception.CancelledException;
@@ -54,15 +68,22 @@ public class XbeXbSymbolDatabaseAnalyzer extends AbstractAnalyzer {
 
 	private static final String OPTION_NAME_SET_SYMBOL = "Allow  Set Symbol Name"; // Intentional extra space to force order of list.
 	private static final String OPTION_NAME_DEMANGLE_SYMBOL = "Allow Demangle Symbol Name";
+	private static final String OPTION_NAME_EXTEND_INFORMATION = "Allow Extend Information (includes fixup for param storage and calling convention)";
+	private static final String OPTION_NAME_PARAM_VARS_RENAME = "EXTEND: Allow Rename Param Variables";
 
 	private static final boolean OPTION_DEFAULT_ALLOW_SET_SYMBOL_NAME = true;
 	private static final boolean OPTION_DEFAULT_ALLOW_DEMANGLE_NAME = false;
+	private static final boolean OPTION_DEFAULT_ALLOW_EXTEND_INFORMATION = true;
 
 	private boolean allowSetSymbolName = OPTION_DEFAULT_ALLOW_SET_SYMBOL_NAME;
 	private boolean allowDemangleName = OPTION_DEFAULT_ALLOW_DEMANGLE_NAME;
+	private boolean allowExtendInformation = OPTION_DEFAULT_ALLOW_EXTEND_INFORMATION;
+	private boolean allowParameterStorageFixup = OPTION_DEFAULT_ALLOW_EXTEND_INFORMATION;
+	private boolean allowParameterRename = !OPTION_DEFAULT_ALLOW_EXTEND_INFORMATION;
 
 	public XbeXbSymbolDatabaseAnalyzer() {
 		super("Xbox Symbol Database Analyzer", "Scan XBE for known library functions", AnalyzerType.BYTE_ANALYZER);
+		setPriority(AnalysisPriority.DISASSEMBLY.before());
 	}
 
 	@Override
@@ -80,12 +101,16 @@ public class XbeXbSymbolDatabaseAnalyzer extends AbstractAnalyzer {
 		// NOTE: Ghidra does not keep the options listing in order.
 		options.registerOption(OPTION_NAME_SET_SYMBOL, allowSetSymbolName, null, "Allow set symbol name, otherwise perform fixups if needed. (Demangle symbol will be excluded.)");
 		options.registerOption(OPTION_NAME_DEMANGLE_SYMBOL, allowDemangleName, null, "Demangle symbol name into generic symbol name.");
+		options.registerOption(OPTION_NAME_EXTEND_INFORMATION, allowExtendInformation, null, "Use extended information supplied by reverse-engineered information output.");
+		options.registerOption(OPTION_NAME_PARAM_VARS_RENAME, allowParameterRename, null, "Allow rename parameters by XbSymbolDatabase supplied information.");
 	}
 
 	@Override
 	public void optionsChanged(Options options, Program program) {
 		allowSetSymbolName = options.getBoolean(OPTION_NAME_SET_SYMBOL, OPTION_DEFAULT_ALLOW_SET_SYMBOL_NAME);
 		allowDemangleName = options.getBoolean(OPTION_NAME_DEMANGLE_SYMBOL, OPTION_DEFAULT_ALLOW_DEMANGLE_NAME);
+		allowExtendInformation = options.getBoolean(OPTION_NAME_EXTEND_INFORMATION, OPTION_DEFAULT_ALLOW_EXTEND_INFORMATION);
+		allowParameterRename = options.getBoolean(OPTION_NAME_PARAM_VARS_RENAME, OPTION_DEFAULT_ALLOW_EXTEND_INFORMATION);
 	}
 
 	@Override
@@ -99,7 +124,7 @@ public class XbeXbSymbolDatabaseAnalyzer extends AbstractAnalyzer {
 		} else {
 			toolExec = xbsdb_tool_exec;
 		}
-		
+
 		String toolPath;
 		try {
 			toolPath = Application.getOSFile(toolExec).getAbsolutePath();
@@ -115,12 +140,15 @@ public class XbeXbSymbolDatabaseAnalyzer extends AbstractAnalyzer {
 				xbePath = xbePath.substring(1).replace("/", "\\");
 			}
 		}
-		
+
 		List<String> cmd = new ArrayList<>();
 		cmd.add(toolPath);
 		cmd.add(xbePath);
 		if (allowDemangleName) {
 			cmd.add("-d");
+		}
+		if (allowExtendInformation) {
+			cmd.add("-e");
 		}
 
 		try {
@@ -130,16 +158,42 @@ public class XbeXbSymbolDatabaseAnalyzer extends AbstractAnalyzer {
 			String line;
 			while ((line = output.readLine()) != null) {
 				String[] params = line.split("=");
+				boolean isFunction = false;
 				Address address = api.toAddr(Long.decode(params[1].strip()));
 				String fullName = params[0].strip();
 				int libNameLength = fullName.indexOf("__");
-				String lib = fullName.substring(0, libNameLength);
-				String name = fullName.substring(libNameLength+2);
-				if (allowSetSymbolName) {
-					program.getSymbolTable().createLabel(address, name, getNamespace(program, lib), SourceType.ANALYSIS);
+				String libName = fullName.substring(0, libNameLength);
+				String symbolType, symbolName, callType = null;
+				// Check if the extend information option is enabled, then process additional information supplied by xbsdb.
+				if (allowExtendInformation) {
+					int libTypeLength = fullName.indexOf("__", libNameLength + 2);
+					symbolType = fullName.substring(libNameLength + 2, libTypeLength);
+					// Check if symbol type is a function.
+					if (symbolType.equals("FUN")) {
+						isFunction = true;
+						int callTypeLength = fullName.indexOf("__", libTypeLength + 2);
+						callType = fullName.substring(libTypeLength + 2, callTypeLength);
+						symbolName = fullName.substring(callTypeLength + 2, fullName.lastIndexOf("("));
+					}
+					// Otherwise, consider it as a variable.
+					else {
+						symbolName = fullName.substring(libNameLength + 2);
+					}
+				}
+				// Otherwise, get the symbol name.
+				else {
+					symbolName = fullName.substring(libNameLength + 2);
+				}
+
+				if (isFunction) {
+					processSymbolFunction(program, api, log, address, symbolName, getNamespace(program, libName), callType, fullName);
+				}
+				else if (allowSetSymbolName) {
+					// NOTE: symbolName cannot be null to create label for variables. So, we must check if allowSetSymbolName is true first.
+					program.getSymbolTable().createLabel(address, symbolName, getNamespace(program, libName), SourceType.ANALYSIS);
 				}
 			}
-			
+
 			exec.waitFor();
 		} catch (Throwable e) {
 			int st_i = 0;
@@ -153,6 +207,157 @@ public class XbeXbSymbolDatabaseAnalyzer extends AbstractAnalyzer {
 		}
 
 		return true;
+	}
+
+	private void processSymbolFunction(Program program, FlatProgramAPI api, MessageLog log, Address address, String symbolName, Namespace libName, String callType, String fullName) {
+		String ghidraSymbol = "N/A";
+		String ghidraSymbolPrototype = "N/A";
+		Function func = api.getFunctionAt​(address);
+		Parameter ghidraParams[] = new Parameter[0];
+		try {
+			// Check if Ghidra had the function created or not.
+			if (func == null) {
+				func = api.createFunction(address, (allowSetSymbolName ? symbolName : null));
+			}
+			else {
+				// Generate symbol mockup with parameters in case of exception occurs for readability.
+				ghidraSymbol = func.getCallingConventionName() + " " + func.getName() + "(";
+				ghidraParams = func.getParameters(VariableFilter.PARAMETER_FILTER);
+				int ghidraParamsCount = func.getParameterCount();
+				int ghidraParamsAutoCount = func.getAutoParameterCount();
+				ghidraSymbolPrototype = func.getPrototypeString(true, true);
+				String paramSymbols = new String();
+				for (Variable param : ghidraParams) {
+					VariableStorage paramStorage = param.getVariableStorage();
+					if (!paramSymbols.isEmpty()) {
+						paramSymbols += ",";
+					}
+					paramSymbols += paramStorage + " " + param.getName();
+				}
+				ghidraSymbol += paramSymbols + "); parameter count: " + ghidraParamsCount + ", auto parameter count: " + ghidraParamsAutoCount;
+				// Check if user has request to set symbol name and update if both names are not the same.
+				if (allowSetSymbolName && !func.getName().equals(symbolName)) {
+					func.setName(symbolName, SourceType.ANALYSIS);
+				}
+			}
+			Namespace ghidraLibName = func.getParentNamespace();
+			if (ghidraLibName == null || ghidraLibName.getID() != libName.getID()) {
+				func.setParentNamespace(libName);
+			}
+			boolean doUpdateParams = false;
+			if (allowExtendInformation) {
+				String[] xbsdbParams = fullName.substring(fullName.indexOf("(") + 1, fullName.indexOf(")")).split(",");
+				if (xbsdbParams[0].isEmpty()) {
+					return;
+				}
+
+				Parameter updateParams[] = new Parameter[xbsdbParams.length];
+				int paramOffset = 0;
+				int pushOffset = 4; // Always default to 4.
+				for (String param : xbsdbParams) {
+					String storageName[] = param.strip().split(" ");
+					Parameter ghidraParam;
+					DataType paramType;
+					VariableStorage paramStorage;
+					String paramName;
+					boolean doUpdateParam = false;
+					// Check if Ghidra's parameters are within bound.
+					if (paramOffset < ghidraParams.length) {
+						ghidraParam = ghidraParams[paramOffset];
+						paramName = ghidraParam.getName();
+						paramType = ghidraParam.getDataType();
+						paramStorage = ghidraParam.getVariableStorage();
+					}
+					// Otherwise use default value for missing parameter.
+					else {
+						ghidraParam = null;
+						paramName = null;
+						paramType = Undefined.getUndefinedDataType(4);
+						paramStorage = VariableStorage.UNASSIGNED_STORAGE;
+					}
+					// Check if user has request to verify and perform parameter storage fixup.
+					if (allowParameterStorageFixup) {
+						VariableStorage storageUpdate;
+						DataType typeUpdate;
+						// Create push variable to verify supplied input from Ghidra.
+						if (storageName[0].startsWith("psh")) {
+							String multiPush = storageName[0].substring(3);
+							int size = 4;
+							// Check multi-push from "psh#" input, then multiply it with the size of 4.
+							if (!multiPush.isEmpty()) {
+								size *= Integer.parseInt(multiPush);
+							}
+							storageUpdate = new VariableStorage(program, pushOffset, size);
+							typeUpdate = Undefined.getUndefinedDataType(size);
+							pushOffset += size;
+						}
+						// Create register variable to verify supplied input from Ghidra.
+						else {
+							Register register = getStorageType(program, storageName[0].toUpperCase());
+							storageUpdate = new VariableStorage(program, register);
+							typeUpdate = Undefined.getUndefinedDataType(register.getMinimumByteSize());
+						}
+						// Fix variable storage if it is not the same.
+						if (storageUpdate.compareTo(paramStorage) != 0) {
+							paramStorage = storageUpdate;
+							doUpdateParam = true;
+						}
+						// Fix the data type if it is not the same.
+						if (Undefined.isUndefined(paramType) && typeUpdate.getLength() != paramType.getLength()) {
+							paramType = typeUpdate;
+							doUpdateParam = true;
+						}
+						// Create a new parameter to replace the existing one or add a parameter to the list.
+						if (doUpdateParam) {
+							ghidraParam = new ParameterImpl(paramName, paramType, paramStorage, program, SourceType.DEFAULT);
+							doUpdateParams = true;
+						}
+					}
+					// Check if user has request to set parameter name and update if both names are not the same.
+					if (allowParameterRename && 1 < storageName.length) {// once parameters listing is fixed again, enable -> && ghidraParam != null) {
+						if (!storageName[1].equals(ghidraParam.getName())) {
+							ghidraParam.setName(storageName[1], SourceType.ANALYSIS);
+							doUpdateParams = true;
+						}
+					}
+					updateParams[paramOffset] = ghidraParam;
+					paramOffset++;
+				}
+				// If any above changes occur, override Ghidra's input with our own correction.
+				if (doUpdateParams) {
+					ghidraParams = updateParams;
+				}
+			}
+			// Update the parameters on request.
+			if (doUpdateParams) {
+				GenericCallingConvention ghidraCallType = GenericCallingConvention.getGenericCallingConvention(callType);
+				func.setCallingConvention(ghidraCallType.toString());
+				FunctionUpdateType ghidraStorageType;
+				if (ghidraCallType == GenericCallingConvention.unknown) {
+					ghidraStorageType = FunctionUpdateType.CUSTOM_STORAGE;
+				}
+				else {
+					ghidraStorageType = FunctionUpdateType.DYNAMIC_STORAGE_ALL_PARAMS;
+				}
+				func.replaceParameters(ghidraStorageType, true, SourceType.ANALYSIS, ghidraParams);
+				// Anything else need to be put in here? i.e. decompile again, etc
+			}
+		} catch (Throwable e) {
+			log.appendMsg("xbsdb symbol : " + fullName);
+			log.appendMsg("ghidra symbol: " + ghidraSymbol);
+			log.appendMsg("ghidra proto : " + ghidraSymbolPrototype);
+			int st_i = 0;
+			for (StackTraceElement stackTrace : e.getStackTrace()) {
+				log.appendMsg("stack[" + st_i + "]     : " + stackTrace.toString());
+				st_i++;
+			}
+			log.appendMsg("message      : " + e.getMessage());
+			log.appendMsg("--------------");
+		}
+	}
+
+	private Register getStorageType(Program program, String storageName) {
+		return program.getProgramContext().getRegister(storageName);
 	}
 
 	private Namespace getNamespace(Program program, String namespace) {
